7969ea8d6ce3a5efd55c5d837204d17653564fa8
melkemk <melkemk503@gmail.com>
2022-11-30 13:55:26 +0300

    first commit

---
+++ MyFirstJava.java
@@ -0,0 +1,13 @@
+public class MyFirstJava{
+public static void main(String[] args){
+	System.out.println("Hello");
+	
+	String text ="hello";
+	//System.out.println(text[0]);
+	System.out.println(text+"world ");
+int[] lmyArray={2,3,4};
+System.out.println(lmyArray[0]);
+	
+}
+
+}
