b5d7a3c0297689f1c9b3ce1dd616fc2b6e6ce274
melkemk <87375258+melkemk@users.noreply.github.com>
2023-12-12 17:41:31 +0300

    Added solution for Card Flipping Game

--- python/week5/card-flipping-game.py
+++ python/week5/card-flipping-game.py
@@ -1,12 +1,10 @@
 class Solution:
     def flipgame(self, fronts: List[int], backs: List[int]) -> int:
-        i=len(fronts)-1
         possible=set(sorted(set(fronts) | set(backs)))
-        while(i>=0):
+        for i in range(len(fronts)):
             if fronts[i]==backs[i]:
                 possible.discard(fronts[i])
-            i-=1
-        if len(possible):return next(iter(possible))
-        return 0
+        return next(iter(possible)) if possible else 0
+        
 
 
