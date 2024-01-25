b14274c18e4e4b462a4495eb0ff1bfbbc9adb9b8
melkemk <87375258+melkemk@users.noreply.github.com>
2024-01-25 19:00:05 +0300

    Added solution for Insertion Sort List

---
+++ linked-list/insertion-sort-list.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val = 0, next = None):
+#         self.val  =  val
+#         self.next  =  next
+class Solution:
+    def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        anstrav = ListNode()
+        trav = head
+        ans=[]
+        while trav:
+            ans.append(trav.val)
+            trav=trav.next
+        ans.sort()
+        tr=ListNode()
+        anss=tr
+        for i in ans:
+            tr.next=ListNode(i)
+            tr=tr.next
+        return anss.next
