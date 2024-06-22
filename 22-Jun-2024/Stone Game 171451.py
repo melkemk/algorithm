fad2a7d9c6a4b7feee97fbd5d2035b48481e8900
melkemk <87375258+melkemk@users.noreply.github.com>
2024-06-22 18:51:06 +0600

    Created 'Stone Game' by A2SV Hub

---
+++ 22-Jun-2024/Stone Game 171451.py
@@ -0,0 +1,6 @@
+# Problem: Stone Game - https://leetcode.com/problems/stone-game/
+
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        
+        return sum(piles)%2 if piles[0] else 1 
